d3a35f894c92b3d348b4dc12b88d12f9852f10df
princeksp <mis918772@gmail.com>
2025-07-24 06:47:02 +0530

    Function In JS

---
+++ day-9 Function In JS/script.js
@@ -0,0 +1,21 @@
+/**
+ * ! 1. Function Declaration Statement
+
+
+function sum(a,b)  // Parameters
+{
+    console.log("Sum:",a + b);
+}
+
+
+sum(10,10)  // Arguments
+console.log(sum);
+ */
+
+function meme(user) {
+  console.log(`${user} Smjh rhe ho..`);
+  // return 10000
+}
+
+const x = meme("Dinga");
+console.log("x:", x);
