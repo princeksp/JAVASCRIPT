8c845b09fe7b326887ae6feb3e459450183eec1a
princeksp <mis918772@gmail.com>
2025-07-26 10:14:18 +0530

    function in js

--- day-9 Function In JS/script.js
+++ day-9 Function In JS/script.js
@@ -12,10 +12,63 @@ sum(10,10)  // Arguments
 console.log(sum);
  */
 
-function meme(user) {
-  console.log(`${user} Smjh rhe ho..`);
-  // return 10000
+/**
+ * ! Example-1
+ 
+console.log("start")
+function greet(user)
+{
+    console.log(`GM ${user}`);
 }
 
-const x = meme("Dinga");
-console.log("x:", x);
+const x=greet("Raj");
+console.log("x:",x);
+console.log("end");
+*/
+
+/**
+ * ! Example-2
+
+
+console.log("start");
+
+function sum(a,b)
+{
+    const total=a + b;
+    console.log("Total is:",total);
+    return total;
+}
+
+const x=sum(10,10);
+console.log("x:",x);
+console.log("end");
+ */
+
+/**
+ * ! Example-3
+
+console.log("Start");
+const a=10;
+
+greet("Aman");
+
+function greet(user)
+{
+    console.log(`Namaste ${user}`);
+    console.log(a); // 10
+}
+
+console.log(greet)
+console.log("end") */
+
+/**
+ * ! Default Parameter
+
+
+function dialogue(user="Chombu")
+{
+    console.log(`Hey ${user}`);
+}
+
+dialogue("Raj");
+ */
